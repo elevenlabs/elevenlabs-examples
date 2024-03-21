e133f387ef43db9728627c6398817d700b06536f
Marcel Thomas <marcelthomas5@gmail.com>
2024-03-21 14:20:50 +0000

    Added elevenlabs alpha

--- examples/websockets/servers/node/src/index.ts
+++ examples/websockets/servers/node/src/index.ts
@@ -1,39 +1,37 @@
 import 'dotenv/config';
 import express from 'express';
-import http from 'http';
+import ExpressWs from 'express-ws';
 import cors from 'cors';
 import helmet from 'helmet';
-import { Server } from 'socket.io';
-import { websocket } from './websocket';
+import { WebSocket } from 'ws';
+import { ElevenLabsAlpha } from 'elevenlabs-alpha';
 
-const port = Number(process.env.PORT) || 3001;
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
-});
+const app = ExpressWs(express()).app;
+const PORT: number = parseInt(process.env.PORT || '5000');
 
-(async () => {
-  app.use(cors());
-  app.use(helmet());
-  app.use(express.json());
+const elevenlabs = new ElevenLabsAlpha();
 
-  io.on('connection', (socket) => {
-    socket.on('init', (text) => {
-      console.log('socket init');
-      websocket(text, (audio) => {
-        socket.send(audio);
-      });
+app.use(cors());
+app.use(helmet());
+
+// realtime audio
+app.ws('/text-to-speech/realtime', (ws: WebSocket) => {
+  ws.on('error', console.error);
+
+  ws.on('message', (text: string) => {
+    elevenlabs.textToSpeech.realtime({
+      text,
+      onAudioReceived: (audio) => {
+        ws.send(audio);
+      },
     });
   });
+});
 
-  app.use('*', (req, res) => {
-    res.status(404).json({ error: 'Not found' });
-  });
+app.use('*', (req, res) => {
+  res.status(404).json({ error: 'Not found' });
+});
 
-  server.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-  });
-})();
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
