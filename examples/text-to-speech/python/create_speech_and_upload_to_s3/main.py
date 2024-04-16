374139209f3160e5bebbd2172de23d65d565cf39
devman0129 <oleksandrkliatskyi.dev@outlook.com>
2024-04-16 10:33:30 -0400

    fix: remove bucket_name from arguments

--- examples/text-to-speech/python/create_speech_and_upload_to_s3/main.py
+++ examples/text-to-speech/python/create_speech_and_upload_to_s3/main.py
@@ -10,11 +10,10 @@ from s3_uploader import upload_audiostream_to_s3, generate_presigned_url
 
 def main():
     text = "This is James"
-    AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
 
     audio_stream = text_to_speech_stream(text)
-    s3_file_name = upload_audiostream_to_s3(audio_stream, AWS_S3_BUCKET_NAME)
-    signed_url = generate_presigned_url(AWS_S3_BUCKET_NAME, s3_file_name)
+    s3_file_name = upload_audiostream_to_s3(audio_stream)
+    signed_url = generate_presigned_url(s3_file_name)
 
     print(f"Signed URL to access the file: {signed_url}")
 
--- examples/text-to-speech/python/create_speech_and_upload_to_s3/s3_uploader.py
+++ examples/text-to-speech/python/create_speech_and_upload_to_s3/s3_uploader.py
@@ -7,6 +7,7 @@ import uuid
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION_NAME = os.getenv("AWS_REGION_NAME")
+AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
 
 session = boto3.Session(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -16,12 +17,11 @@ session = boto3.Session(
 s3 = session.client("s3")
 
 
-def generate_presigned_url(bucket_name: str, s3_file_name: str) -> str:
+def generate_presigned_url(s3_file_name: str) -> str:
     """
     Generates a presigned URL for an S3 object allowing temporary access without AWS credentials.
 
     Args:
-        bucket_name (str): The name of the S3 bucket.
         s3_file_name (str): The key name of the S3 object for which to generate the presigned URL.
 
     Returns:
@@ -29,24 +29,23 @@ def generate_presigned_url(bucket_name: str, s3_file_name: str) -> str:
     """
     signed_url = s3.generate_presigned_url(
         "get_object",
-        Params={"Bucket": bucket_name, "Key": s3_file_name},
+        Params={"Bucket": AWS_S3_BUCKET_NAME, "Key": s3_file_name},
         ExpiresIn=3600,
     )  # URL expires in 1 hour
     return signed_url
 
 
-def upload_audiostream_to_s3(audio_stream, bucket_name: str) -> str:
+def upload_audiostream_to_s3(audio_stream) -> str:
     """
     Uploads an audio stream to an S3 bucket.
 
     Args:
         audio_stream: The audio stream (file-like object) to be uploaded.
-        bucket_name (str): The name of the S3 bucket where the audio stream will be uploaded.
 
     Returns:
         str: The unique S3 file name under which the audio stream was saved.
     """
     s3_file_name = f"{uuid.uuid4()}.mp3"  # Generates a unique file name using UUID
-    s3.upload_fileobj(audio_stream, bucket_name, s3_file_name)
+    s3.upload_fileobj(audio_stream, AWS_S3_BUCKET_NAME, s3_file_name)
 
     return s3_file_name
