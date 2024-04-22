777b86b2d9ff16a6c83a1aba9722b70b5ac762c4
Luke Harries <lharries@users.noreply.github.com>
2024-04-22 22:26:51 +0100

    clean

--- examples/pronunciation-dictionaries/python/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Get metadata for a pronunciation dictionary (GET /v1/pronunciation-dictionaries/:pronunciation_dictionary_id)
-response = requests.get(
-    "https://api.elevenlabs.io/v1/pronunciation-dictionaries/6wdDtkYfgbKgzImFXQMM",
-    headers={"xi-api-key": "33afe1de43d8565797378c1e9e85148a"},
-)
-
-print(response.json())
--- examples/text-to-speech/node/src/index.ts
+++ examples/text-to-speech/node/src/index.ts
@@ -4,12 +4,14 @@ import { createAudioStreamFromText } from "./text_to_speech_stream";
 import { generatePresignedUrl, uploadAudioStreamToS3 } from "./s3_uploader";
 
 (async () => {
+  // save the audio file to disk
   const fileName = await createAudioFileFromText(
     "Today, the sky is exceptionally clear, and the sun shines brightly."
   );
 
   console.log("File name:", fileName);
 
+  // stream the audio, upload to S3, and get a presigned URL
   const stream = await createAudioStreamFromText(
     "Today, the sky is exceptionally clear, and the sun shines brightly."
   );
