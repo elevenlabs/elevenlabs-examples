6208e899191fed49fd77ddd658444ee025a74455
cahyosubroto <cahyosubroto30@gmail.com>
2024-05-10 17:02:57 +0700

    add dubbing control

--- examples/dubbing/e2e-example/frontend/src/components/layout.tsx
+++ examples/dubbing/e2e-example/frontend/src/components/layout.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Logo } from "./logo";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { Button } from "./ui/button";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
   return (
     <main>
       <div className="w-full flex flex-col flex-wrap max-w-screen-xl mx-auto px-4">
-        <div className="py-4 mb-4 flex gap-x-2 items-baseline border-b-2 border-gray-200">
-          <Link to="/">
-            <Logo />
-          </Link>
-          <p className="text-lg font-sans">| Dubbing Example</p>
+        <div className="py-4 mb-4 flex items-center justify-between border-b-2 border-gray-200">
+          <div className="flex gap-x-2 items-baseline">
+            <Link to="/">
+              <Logo />
+            </Link>
+            <p className="text-lg font-sans">| Dubbing Example</p>
+          </div>
+          <div>
+            {location.pathname !== "/" && (
+              <Button size={"sm"}>
+                <Link to="/">Upload Project</Link>
+              </Button>
+            )}
+          </div>
         </div>
         {children}
       </div>
--- examples/dubbing/e2e-example/frontend/src/pages/stream.tsx
+++ examples/dubbing/e2e-example/frontend/src/pages/stream.tsx
@@ -1,10 +1,11 @@
 import { Layout } from "@/components/layout";
-import { getProject } from "@/services/dubbing";
-import { useEffect, useState } from "react";
+import { getAllProjects, getProject } from "@/services/dubbing";
+import { useEffect, useMemo, useState } from "react";
 import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import VideoPlayer from "@/components/video-player";
-import { AlertCircle, AlertTriangle, Loader2 } from "lucide-react";
+import { AlertCircle, ChevronLeft, ChevronRight, Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const Stream = () => {
   const params = useParams<{ id: string }>();
@@ -15,6 +16,29 @@ export const Stream = () => {
     queryFn: () => getProject(params.id!),
     refetchInterval: shouldRefetch ? 2000 : false, // refetch every 2 seconds
   });
+
+  const { data: projects } = useQuery({
+    queryKey: ["projects"],
+    queryFn: () => getAllProjects(),
+  });
+
+  const prevProject = useMemo(() => {
+    if (!projects || !data) return null;
+    const currentIdx = projects.findIndex(p => p.id === data.id);
+
+    if (currentIdx === 0) return null;
+
+    return projects[currentIdx - 1];
+  }, [projects, data]);
+
+  const nextProject = useMemo(() => {
+    if (!projects || !data) return null;
+    const currentIdx = projects.findIndex(p => p.id === data.id);
+
+    if (currentIdx === projects.length - 1) return null;
+
+    return projects[currentIdx + 1];
+  }, [projects, data]);
 
   useEffect(() => {
     if (data && data.status !== "dubbing") {
@@ -28,6 +52,32 @@ export const Stream = () => {
         <h1 className="font-bold text-xl mb-4">
           Stream Dubbing Project - {data?.name}
         </h1>
+        <div className="w-full flex justify-between">
+          <div>
+            {prevProject && (
+              <Button variant={"link"} className="p-0">
+                <div className="flex items-top gap-x-1">
+                  <ChevronLeft />
+                  <Link to={`/stream/${prevProject.id}`}>
+                    {prevProject.name}
+                  </Link>
+                </div>
+              </Button>
+            )}
+          </div>
+          <div>
+            {nextProject && (
+              <Button variant={"link"} className="p-0">
+                <div className="flex items-top gap-x-1">
+                  <Link to={`/stream/${nextProject.id}`}>
+                    {nextProject.name}
+                  </Link>
+                  <ChevronRight />
+                </div>
+              </Button>
+            )}
+          </div>
+        </div>
         {!data && (
           <div className="mt-[100px]">
             <Loader2 className="animate-spin mx-auto w-8 h-8" />
