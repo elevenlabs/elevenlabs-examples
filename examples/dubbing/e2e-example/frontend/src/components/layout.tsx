7a5b4d2bd8ba14d39f5e4eb181cc6bd7d8d15385
cahyosubroto <cahyosubroto30@gmail.com>
2024-05-10 16:29:58 +0700

    improve ux on create page

--- examples/dubbing/e2e-example/frontend/src/components/layout.tsx
+++ examples/dubbing/e2e-example/frontend/src/components/layout.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Logo } from "./logo";
+import { Link } from "react-router-dom";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <div className="w-full flex flex-col flex-wrap max-w-screen-xl mx-auto px-4">
-        <div className="mt-4 mb-4">
-          <p className="text-3xl font-bold">ElevenVideos</p>
+        <div className="py-4 mb-4 flex gap-x-2 items-baseline border-b-2 border-gray-200">
+          <Link to="/">
+            <Logo />
+          </Link>
+          <p className="text-lg font-sans">| Dubbing Example</p>
         </div>
         {children}
       </div>
--- examples/dubbing/e2e-example/frontend/src/pages/create.tsx
+++ examples/dubbing/e2e-example/frontend/src/pages/create.tsx
@@ -18,6 +18,8 @@ import {
 } from "@/components/ui/select";
 import { toast } from "@/components/ui/use-toast";
 import { addProject } from "@/services/dubbing";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
 import Dropzone from "react-dropzone";
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
@@ -36,6 +38,7 @@ export const Create = () => {
       sourceLang: "detect",
     },
   });
+  const [showError, setShowError] = useState<boolean>(false);
 
   const navigate = useNavigate();
 
@@ -64,120 +67,144 @@ export const Create = () => {
 
   return (
     <Layout>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(val => {
-            mutation.mutate(val);
-          })}
-        >
-          <Dropzone
-            onDrop={acceptedFiles => {
-              if (acceptedFiles.length === 1) {
-                form.setValue("file", acceptedFiles[0]);
+      <div className="mx-auto max-w-screen-md w-full">
+        <h1 className="font-bold text-xl mb-4">Upload Dubbing Project</h1>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(val => {
+              if (!val.file || !val.sourceLang || !val.targetLang) {
+                setShowError(true);
+                return;
               }
-            }}
-            accept={{
-              "video/mp4": [".mp4"],
-            }}
-            maxFiles={1}
+              setShowError(false);
+              mutation.mutate(val);
+            })}
           >
-            {({ getRootProps, getInputProps }) => (
-              <section className="w-full p-8 bg-gray-100 hover:bg-gray-200 rounded-lg">
-                <div {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  {file ? (
-                    <p className="text-center">
-                      Selected <span className="font-bold">{file.name}</span>
-                    </p>
-                  ) : (
-                    <p className="text-center">
-                      Drag 'n' drop a video file here, or click to select file
-                    </p>
-                  )}
-                </div>
-              </section>
+            <h2 className="font-medium text-sm">Source Video</h2>
+            <Dropzone
+              onDrop={acceptedFiles => {
+                if (acceptedFiles.length === 1) {
+                  form.setValue("file", acceptedFiles[0]);
+                }
+              }}
+              accept={{
+                "video/mp4": [".mp4"],
+              }}
+              maxFiles={1}
+            >
+              {({ getRootProps, getInputProps }) => (
+                <section className="w-full p-8 bg-gray-100 hover:bg-gray-200 rounded-lg border-2 hover:border-gray-500 hover:cursor-pointer">
+                  <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    {file ? (
+                      <p className="text-center text-gray-700">
+                        Selected{" "}
+                        <span className="font-medium">{file.name}</span>
+                      </p>
+                    ) : (
+                      <p className="text-center text-gray-700">
+                        Drag 'n' drop a video file here, or click to select file
+                      </p>
+                    )}
+                  </div>
+                </section>
+              )}
+            </Dropzone>
+            <div className="flex gap-x-4 mt-4">
+              <FormField
+                control={form.control}
+                name="sourceLang"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Source Language</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      required
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a language source" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="detect" key={"detect"}>
+                          Auto Detect
+                        </SelectItem>
+                        {languages
+                          .sort((a, b) => a.name.localeCompare(b.name))
+                          .map(language => {
+                            return (
+                              <SelectItem
+                                value={language.code}
+                                key={language.code}
+                              >
+                                {language.countryLogo} {language.name}
+                              </SelectItem>
+                            );
+                          })}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="targetLang"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Target Language</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      required
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a language target" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {languages
+                          .sort((a, b) => a.name.localeCompare(b.name))
+                          .map(language => {
+                            return (
+                              <SelectItem
+                                value={language.code}
+                                key={language.code}
+                              >
+                                {language.countryLogo} {language.name}
+                              </SelectItem>
+                            );
+                          })}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>{" "}
+            {showError && (
+              <p className="mt-2 font-medium text-destructive">
+                *Please fill all the required fields
+              </p>
             )}
-          </Dropzone>
-          <div className="flex gap-x-4 mt-4 justify-center">
-            <FormField
-              control={form.control}
-              name="sourceLang"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Source Language</FormLabel>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a language source" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value="detect" key={"detect"}>
-                        Auto Detect
-                      </SelectItem>
-                      {languages
-                        .sort((a, b) => a.name.localeCompare(b.name))
-                        .map(language => {
-                          return (
-                            <SelectItem
-                              value={language.code}
-                              key={language.code}
-                            >
-                              {language.countryLogo} {language.name}
-                            </SelectItem>
-                          );
-                        })}
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="targetLang"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Target Language</FormLabel>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a language target" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {languages
-                        .sort((a, b) => a.name.localeCompare(b.name))
-                        .map(language => {
-                          return (
-                            <SelectItem
-                              value={language.code}
-                              key={language.code}
-                            >
-                              {language.countryLogo} {language.name}
-                            </SelectItem>
-                          );
-                        })}
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="flex justify-end">
-            <Button type="submit" size={"lg"} disabled={mutation.isLoading}>
-              Submit
-            </Button>
-          </div>
-        </form>
-      </Form>
+            <div className="flex justify-end">
+              <Button type="submit" disabled={mutation.isLoading}>
+                {mutation.isLoading ? (
+                  <div className="flex gap-2 items-center">
+                    <Loader2 className="animate-spin" />
+                    <p>Uploading ...</p>
+                  </div>
+                ) : (
+                  "Submit"
+                )}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Layout>
   );
 };
