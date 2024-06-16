5f773293b9cf442a8b1f39c3b00ba0e63d1573ef
Luke Harries <lharries@users.noreply.github.com>
2024-06-16 18:41:29 -0400

    analytics

--- examples/sound-effects/video-to-sfx/app/layout.tsx
+++ examples/sound-effects/video-to-sfx/app/layout.tsx
@@ -6,10 +6,12 @@ import posthog from "posthog-js";
 
 const queryClient = new QueryClient();
 
-posthog.init("phc_Sr8tySR1UL8RrsY83xDl9AmA1fXyG2gusWsZbTPC16V", {
-  api_host: "https://eu.i.posthog.com",
-  person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-});
+if (typeof window !== "undefined") {
+  posthog.init("phc_Sr8tySR1UL8RrsY83xDl9AmA1fXyG2gusWsZbTPC16V", {
+    api_host: "https://eu.i.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
 
 import { cn } from "@/lib/utils";
 import { ReactNode, useEffect } from "react";
