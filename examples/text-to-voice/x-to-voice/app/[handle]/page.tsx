aceea3f2cd7eade1156df062a2a7c7bc1201ff6f
louisjoecodes <louisjoecodes@gmail.com>
2024-10-24 06:26:49 +0200

    feat: make progress

--- examples/text-to-voice/x-to-voice/app/[handle]/page.tsx
+++ examples/text-to-voice/x-to-voice/app/[handle]/page.tsx
@@ -1,5 +1,18 @@
+"use server";
 import { SpecimenCard } from "@/components/specimen-card";
+import { retrieveHumanSpecimenAction } from "../actions/actions";
+import { notFound } from "next/navigation";
 
-export default function Page({ params }: { params: { handle: string } }) {
-  return <SpecimenCard />;
+export default async function Page({ params }: { params: { handle: string } }) {
+  const paramaters = await params;
+
+  const response = await retrieveHumanSpecimenAction({
+    handle: paramaters.handle,
+  });
+
+  if (!response?.data?.success) {
+    return <>User not found</>;
+  }
+
+  return <SpecimenCard human={response.data.human} />;
 }
