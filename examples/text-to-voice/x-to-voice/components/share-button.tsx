ba3018cbd28beef8822c02ced003981eb126e8e0
Jonatan von Martens <jonatan@elevenlabs.io>
2024-10-29 11:37:56 +0200

    [TTV card] Render on server

---
+++ examples/text-to-voice/x-to-voice/components/share-button.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Link2 } from "lucide-react";
+import { toast } from "sonner";
+
+export function ShareOnXButton({ shareText }: { shareText: string }) {
+  const url = window.location.href;
+
+  function share() {
+    window.open(
+      `https://x.com/intent/tweet?text=${encodeURIComponent(
+        shareText,
+      )}&url=${encodeURIComponent(url)}`,
+    );
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => share()}
+    >
+      <Image
+        src="/x.png"
+        alt="X (formerly Twitter) logo"
+        width={20}
+        height={20}
+        className="w-4 h-4"
+      />
+    </Button>
+  );
+}
+
+export function CopyShareLink() {
+  const url = window.location.href;
+  function copyLink() {
+
+    navigator.clipboard.writeText(url);
+    toast.info("Link copied to clipboard 📋");
+  }
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => copyLink()}
+    >
+      <Link2 className="w-4 h-4" />
+    </Button>
+  );
+}
