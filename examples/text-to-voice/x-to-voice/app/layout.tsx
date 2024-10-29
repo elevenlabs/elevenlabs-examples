533ff7679bd32a26e11ae601a113c1153e88cdc7
Jonatan von Martens <jonatan@elevenlabs.io>
2024-10-29 18:51:15 +0200

    feat: add header

--- examples/text-to-voice/x-to-voice/app/layout.tsx
+++ examples/text-to-voice/x-to-voice/app/layout.tsx
@@ -1,4 +1,4 @@
-export const maxDuration = 60; // Applies to the actions
+import { ElevenLabsLogo, GithubLogo } from "@/components/logos";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -6,8 +6,9 @@ import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/react";
 import { BackgroundWave } from "@/components/background-wave";
 import Link from "next/link";
-import { Github } from "lucide-react";
 import { Button } from "@/components/ui/button";
+
+export const maxDuration = 60; // Applies to the actions
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,38 +27,52 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <body
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+    <nav className={"sm:fixed w-full top-0 left-0 flex items-center justify-between py-4 px-8"}>
+      <div className={"flex"}>
+        <Link href={"/"}><ElevenLabsLogo className={"h-[15px] w-auto hover:text-gray-500"} /></Link>
+      </div>
+      <div className={"flex gap-4"}>
+        <Link
+          href="https://elevenlabs.io/app/sign-in"
+          target="_blank"
+          aria-label="View source on GitHub"
+        >
+          <Button
+            variant={"secondary"}
+            size={"xs"}
+            className="rounded-full z-50 text-sm text-gray-800"
+          >
+              Sign up
+          </Button>
+        </Link>
         <Link
           href="https://github.com/elevenlabs/elevenlabs-examples/tree/main/examples/text-to-voice/x-to-voice"
           target="_blank"
           rel="noopener noreferrer"
+          className={"py-0.5"}
           aria-label="View source on GitHub"
         >
-          <Button
-            variant="ghost"
-            className="fixed top-4 right-4 flex items-center gap-2 p-2 pr-3 rounded-lg z-50 text-sm font-medium"
-          >
-            <Github className="w-5 h-5" />
-            <span className="opacity-50 hover:opacity-100 transition-opacity">
-              Open source
-            </span>
-          </Button>
+          <GithubLogo className={"w-5 h-5 hover:text-gray-500 text-[#24292f]"}></GithubLogo>
         </Link>
-        <div className="flex flex-col min-h-screen w-full items-center justify-center px-4">
-          {children}
-          <BackgroundWave />
-        </div>
-        <Toaster />
-        <Analytics />
-      </body>
+      </div>
+    </nav>
+
+    <div className="flex flex-col min-h-screen w-full items-center justify-center px-4">
+      {children}
+      <BackgroundWave />
+    </div>
+    <Toaster />
+    <Analytics />
+    </body>
     </html>
   );
 }
