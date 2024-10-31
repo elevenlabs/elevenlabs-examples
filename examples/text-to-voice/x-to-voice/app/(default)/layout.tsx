c7b390b175d7cd5a8a2f991929da49f21e26906b
louisjoecodes <louisjoecodes@gmail.com>
2024-10-31 18:53:10 +0100

    feat: add vercel speed insights

--- examples/text-to-voice/x-to-voice/app/(default)/layout.tsx
+++ examples/text-to-voice/x-to-voice/app/(default)/layout.tsx
@@ -7,6 +7,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { BackgroundWave } from "@/components/background-wave";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const maxDuration = 60; // Applies to the actions
 
@@ -23,54 +24,66 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "X to Voice | ElevenLabs",
-  description: "Analyze your X profile to generate a unique voice using ElevenLabs' new Voice Design feature",
+  description:
+    "Analyze your X profile to generate a unique voice using ElevenLabs' new Voice Design feature",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={"h-full w-full"}>
-    <body
-      className={`${geistSans.variable} ${geistMono.variable} antialiased w-full h-full flex flex-col sm:pt-20`}
-    >
-    <nav className={"sm:fixed w-full top-0 left-0 flex items-center justify-between py-4 px-8"}>
-      <div className={"flex"}>
-        <Link href={"/"} prefetch={true}><ElevenLabsLogo
-          className={"h-[15px] w-auto hover:text-gray-500"} /></Link>
-      </div>
-      <div className={"flex gap-4"}>
-        <Link
-          href="https://elevenlabs.io/app/sign-in"
-          target="_blank"
-          aria-label="View source on GitHub"
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full h-full flex flex-col sm:pt-20`}
+      >
+        <nav
+          className={
+            "sm:fixed w-full top-0 left-0 flex items-center justify-between py-4 px-8"
+          }
         >
-          <Button
-            variant={"secondary"}
-            size={"xs"}
-            className="rounded-full z-50 text-sm text-gray-800"
-          >
-            Sign up
-          </Button>
-        </Link>
-        <Link
-          href="https://github.com/elevenlabs/elevenlabs-examples/tree/main/examples/text-to-voice/x-to-voice"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={"py-0.5"}
-          aria-label="View source on GitHub"
-        >
-          <GithubLogo className={"w-5 h-5 hover:text-gray-500 text-[#24292f]"}></GithubLogo>
-        </Link>
-      </div>
-    </nav>
+          <div className={"flex"}>
+            <Link href={"/"} prefetch={true}>
+              <ElevenLabsLogo
+                className={"h-[15px] w-auto hover:text-gray-500"}
+              />
+            </Link>
+          </div>
+          <div className={"flex gap-4"}>
+            <Link
+              href="https://elevenlabs.io/app/sign-in"
+              target="_blank"
+              aria-label="View source on GitHub"
+            >
+              <Button
+                variant={"secondary"}
+                size={"xs"}
+                className="rounded-full z-50 text-sm text-gray-800"
+              >
+                Sign up
+              </Button>
+            </Link>
+            <Link
+              href="https://github.com/elevenlabs/elevenlabs-examples/tree/main/examples/text-to-voice/x-to-voice"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={"py-0.5"}
+              aria-label="View source on GitHub"
+            >
+              <GithubLogo
+                className={"w-5 h-5 hover:text-gray-500 text-[#24292f]"}
+              />
+            </Link>
+          </div>
+        </nav>
 
-    <div className="flex flex-col flex-grow w-full items-center justify-center sm:px-4">
-      {children}
-      <BackgroundWave />
-    </div>
-    <Toaster />
-    <Analytics />
-    </body>
-
+        <div className="flex flex-col flex-grow w-full items-center justify-center sm:px-4">
+          {children}
+          <BackgroundWave />
+        </div>
+        <Toaster />
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
