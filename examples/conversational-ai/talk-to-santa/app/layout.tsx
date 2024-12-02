69b30f7627424d472f60dfcf7769b838f614ae67
Louis Jordan <louisjoecodes@gmail.com>
2024-12-02 15:41:43 +0000

    feat: add analytics talk-to-santa

--- examples/conversational-ai/talk-to-santa/app/layout.tsx
+++ examples/conversational-ai/talk-to-santa/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
