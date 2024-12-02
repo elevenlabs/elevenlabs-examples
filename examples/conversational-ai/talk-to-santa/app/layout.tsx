425881acb1dabd2983674edfc2ba66a4691211f1
Louis Jordan <louisjoecodes@gmail.com>
2024-12-02 18:59:38 +0000

    feat: talk to santa

--- examples/conversational-ai/talk-to-santa/app/layout.tsx
+++ examples/conversational-ai/talk-to-santa/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
-      <Analytics />
     </html>
   );
 }
