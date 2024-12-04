ca62ec811547505a1ad54ff6ecb792b820e1ff7c
Louis Jordan <louisjoecodes@gmail.com>
2024-12-04 16:44:56 +0000

    feat: correct param id

--- examples/conversational-ai/talk-to-santa/app/(main)/(santa)/cards/[id]/layout.tsx
+++ examples/conversational-ai/talk-to-santa/app/(main)/(santa)/cards/[id]/layout.tsx
@@ -6,15 +6,16 @@ export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
-  params: { id: string };
 }) {
   return (
-    <Suspense fallback={
-      <div className="flex flex-col items-center justify-center mt-8">
-        <Loader2 className="text-white p-4 rounded-full h-20 w-20" />
-        <span className="mt-2 text-white font-bold">Loading card...</span>
-      </div>
-    }>
+    <Suspense
+      fallback={
+        <div className="flex flex-col items-center justify-center mt-8">
+          <Loader2 className="text-white p-4 rounded-full h-20 w-20" />
+          <span className="mt-2 text-white font-bold">Loading card...</span>
+        </div>
+      }
+    >
       {children}
     </Suspense>
   );
