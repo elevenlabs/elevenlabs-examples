00a2d877482298a58ccfecf03569b9986bfe3826
thorwebdev <thor@elevenlabs.io>
2025-02-14 17:18:31 +0800

    chore: fix double quotes.

--- examples/text-to-speech/deno/mod.ts
+++ examples/text-to-speech/deno/mod.ts
@@ -2,11 +2,11 @@ import { createAudioFileFromText } from "./text-to-speech/text-to-speech-file.ts
 import { createAudioFromTextToS3 } from "./text-to-speech/text-to-speech-aws.ts";
 
 await createAudioFileFromText(
-  '"Today, the sky is exceptionally clear, and the sun shines brightly."',
+  "Today, the sky is exceptionally clear, and the sun shines brightly.",
   "output.mp3"
 );
 
 await createAudioFromTextToS3(
-  '"Today, the sky is exceptionally clear, and the sun shines brightly."',
+  "Today, the sky is exceptionally clear, and the sun shines brightly.",
   "output.mp3"
 );
