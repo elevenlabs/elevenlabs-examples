ea6bbd7976b919a0fa3cbf4a68971e7afc2cdb37
thorwebdev <thor@elevenlabs.io>
2025-02-14 15:30:03 +0800

    fix: deno examples.

--- examples/text-to-speech/deno/mod.ts
+++ examples/text-to-speech/deno/mod.ts
@@ -1,3 +1,12 @@
-import { createAudioFileFromText } from './text-to-speech/text-to-speech-file.ts';
+import { createAudioFileFromText } from "./text-to-speech/text-to-speech-file.ts";
+import { createAudioFromTextToS3 } from "./text-to-speech/text-to-speech-aws.ts";
 
-await createAudioFileFromText('This is a test.', 'output.mp3');
+await createAudioFileFromText(
+  '"Today, the sky is exceptionally clear, and the sun shines brightly."',
+  "output.mp3"
+);
+
+await createAudioFromTextToS3(
+  '"Today, the sky is exceptionally clear, and the sun shines brightly."',
+  "output.mp3"
+);
