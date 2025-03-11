227dbadad82aa8fad1d378cf3454039988496254
thorwebdev <thor@elevenlabs.io>
2025-03-12 00:51:57 +0800

    feat: add text-to-voice node demo.

---
+++ examples/text-to-voice/node/src/index.ts
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv";
+import { ElevenLabsClient, play } from "elevenlabs";
+import { Readable } from "stream";
+
+dotenv.config();
+const client = new ElevenLabsClient();
+
+(async () => {
+  const voices = await client.textToVoice.createPreviews({
+    voice_description: "An old english wizard with a thick accent",
+    text: "Are you ready to be amazed my little muggle friends? The show is about to start and I'm going to show you some wild tricks!",
+    // New model params
+    loudness: 1,
+    quality: 0.3,
+    guidance_scale: 50,
+    seed: 42, // Random number that controls the voice generation. Same seed with same inputs produces same voice.
+  });
+  if (voices?.previews.length > 0) {
+    // Loop through the voices and play the audio
+    for (const voice of voices.previews) {
+      const audio = voice.audio_base_64;
+      if (audio) {
+        await play(Readable.from(Buffer.from(audio, "base64")));
+      }
+    }
+  }
+})();
