f18b26e78467bc2785b64b8161243a4414e62899
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2025-03-21 23:32:48 +0800

    chore: updates. (#148)

--- examples/conversational-ai/nextjs/components/ConvAI.tsx
+++ examples/conversational-ai/nextjs/components/ConvAI.tsx
@@ -1,113 +1,111 @@
-"use client"
+"use client";
 
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import * as React from "react";
-import {useState} from "react";
-import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
-import {Conversation} from "@11labs/client";
-import {cn} from "@/lib/utils";
+import { useCallback } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useConversation } from "@11labs/react";
+import { cn } from "@/lib/utils";
 
 async function requestMicrophonePermission() {
-    try {
-        await navigator.mediaDevices.getUserMedia({audio: true})
-        return true
-    } catch {
-        console.error('Microphone permission denied')
-        return false
-    }
+  try {
+    await navigator.mediaDevices.getUserMedia({ audio: true });
+    return true;
+  } catch {
+    console.error("Microphone permission denied");
+    return false;
+  }
 }
 
 async function getSignedUrl(): Promise<string> {
-    const response = await fetch('/api/signed-url')
-    if (!response.ok) {
-        throw Error('Failed to get signed url')
-    }
-    const data = await response.json()
-    return data.signedUrl
+  const response = await fetch("/api/signed-url");
+  if (!response.ok) {
+    throw Error("Failed to get signed url");
+  }
+  const data = await response.json();
+  return data.signedUrl;
 }
 
 export function ConvAI() {
-    const [conversation, setConversation] = useState<Conversation | null>(null)
-    const [isConnected, setIsConnected] = useState(false)
-    const [isSpeaking, setIsSpeaking] = useState(false)
+  const conversation = useConversation({
+    onConnect: () => {
+      console.log("connected");
+    },
+    onDisconnect: () => {
+      console.log("disconnected");
+    },
+    onError: error => {
+      console.log(error);
+      alert("An error occurred during the conversation");
+    },
+    onMessage: message => {
+      console.log(message);
+    },
+  });
 
-    async function startConversation() {
-        const hasPermission = await requestMicrophonePermission()
-        if (!hasPermission) {
-            alert("No permission")
-            return;
-        }
-        const signedUrl = await getSignedUrl()
-        const conversation = await Conversation.startSession({
-            signedUrl: signedUrl,
-            onConnect: () => {
-                setIsConnected(true)
-                setIsSpeaking(true)
-            },
-            onDisconnect: () => {
-                setIsConnected(false)
-                setIsSpeaking(false)
-            },
-            onError: (error) => {
-                console.log(error)
-                alert('An error occurred during the conversation')
-            },
-            onModeChange: ({mode}) => {
-                setIsSpeaking(mode === 'speaking')
-            },
-        })
-        setConversation(conversation)
+  async function startConversation() {
+    const hasPermission = await requestMicrophonePermission();
+    if (!hasPermission) {
+      alert("No permission");
+      return;
     }
+    const signedUrl = await getSignedUrl();
+    const conversationId = await conversation.startSession({ signedUrl });
+    console.log(conversationId);
+  }
 
-    async function endConversation() {
-        if (!conversation) {
-            return
-        }
-        await conversation.endSession()
-        setConversation(null)
-    }
+  const stopConversation = useCallback(async () => {
+    await conversation.endSession();
+  }, [conversation]);
 
-    return (
-        <div className={"flex justify-center items-center gap-x-4"}>
-            <Card className={'rounded-3xl'}>
-                <CardContent>
-                    <CardHeader>
-                        <CardTitle className={'text-center'}>
-                            {isConnected ? (
-                                isSpeaking ? `Agent is speaking` : 'Agent is listening'
-                            ) : (
-                                'Disconnected'
-                            )}
-                        </CardTitle>
-                    </CardHeader>
-                    <div className={'flex flex-col gap-y-4 text-center'}>
-                        <div className={cn('orb my-16 mx-12',
-                            isSpeaking ? 'animate-orb' : (conversation && 'animate-orb-slow'),
-                            isConnected ? 'orb-active' : 'orb-inactive')}
-                        ></div>
+  return (
+    <div className={"flex justify-center items-center gap-x-4"}>
+      <Card className={"rounded-3xl"}>
+        <CardContent>
+          <CardHeader>
+            <CardTitle className={"text-center"}>
+              {conversation.status === "connected"
+                ? conversation.isSpeaking
+                  ? `Agent is speaking`
+                  : "Agent is listening"
+                : "Disconnected"}
+            </CardTitle>
+          </CardHeader>
+          <div className={"flex flex-col gap-y-4 text-center"}>
+            <div
+              className={cn(
+                "orb my-16 mx-12",
+                conversation.status === "connected" && conversation.isSpeaking
+                  ? "orb-active animate-orb"
+                  : conversation.status === "connected"
+                  ? "animate-orb-slow orb-inactive"
+                  : "orb-inactive"
+              )}
+            ></div>
 
-
-                        <Button
-                            variant={'outline'}
-                            className={'rounded-full'}
-                            size={"lg"}
-                            disabled={conversation !== null && isConnected}
-                            onClick={startConversation}
-                        >
-                            Start conversation
-                        </Button>
-                        <Button
-                            variant={'outline'}
-                            className={'rounded-full'}
-                            size={"lg"}
-                            disabled={conversation === null && !isConnected}
-                            onClick={endConversation}
-                        >
-                            End conversation
-                        </Button>
-                    </div>
-                </CardContent>
-            </Card>
-        </div>
-    )
+            <Button
+              variant={"outline"}
+              className={"rounded-full"}
+              size={"lg"}
+              disabled={
+                conversation !== null && conversation.status === "connected"
+              }
+              onClick={startConversation}
+            >
+              Start conversation
+            </Button>
+            <Button
+              variant={"outline"}
+              className={"rounded-full"}
+              size={"lg"}
+              disabled={conversation === null}
+              onClick={stopConversation}
+            >
+              End conversation
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
