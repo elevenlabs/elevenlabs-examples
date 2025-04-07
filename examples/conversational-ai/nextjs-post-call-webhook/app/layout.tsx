c4f350b6767130218a63c7f11c3610de3911a365
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2025-04-07 18:20:14 +0200

    feat: Next.js ConvAI post call webhook example.  (#153)
    * start
    
    * feat: add knowledge base uploads.
    
    * feat: upload and agent creation.
    
    * feat: add sucess page.
    
    * feat: add email with resend.
    
    * fix: typo.
    
    * chore: cleanup.
    
    * chore: add logo.
    
    * Apply suggestions from code review
    
    Co-authored-by: Paul Asjes <paul.asjes@elevenlabs.io>
    
    * update readme.
    
    ---------
    
    Co-authored-by: Paul Asjes <paul.asjes@elevenlabs.io>

---
+++ examples/conversational-ai/nextjs-post-call-webhook/app/layout.tsx
@@ -0,0 +1,28 @@
+import type React from "react"
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Create your Conversational AI Agent | ElevenLabs",
+  description: "Create a custom AI agent for your website",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
+
+
+
+import './globals.css'
