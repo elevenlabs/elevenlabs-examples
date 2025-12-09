3556b944a21021aec45025b175cdb2c57912c53f
Louis Jordan <louisjoecodes@gmail.com>
2025-12-09 10:40:17 +0000

    chore: update next.config.ts

--- examples/conversational-ai/talk-to-santa/next.config.ts
+++ examples/conversational-ai/talk-to-santa/next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ["raw-loader"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
